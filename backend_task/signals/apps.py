2909ccdf6918df5f925e9c98c72c82f57f2a6b84
Mayur Mali <69722241+mayurmali128@users.noreply.github.com>
2023-05-03 15:31:18 +0530

    Delete backend_task directory

--- backend_task/signals/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class SignalsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'signals'
-    def ready(self):
-        import signals.cao
